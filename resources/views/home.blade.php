76796b7e1300d24ed030e0112877b080d910c1b1
Leonaldi Nata Gunawan <leonaldi.gunawan@xtremax.com>
2023-04-03 19:16:50 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,101 @@
+@extends('layout.mainlayout')
+@section('content')
+
+<div class="container mt-3 mb-3">
+    <div class="row">
+
+        <div class="col">
+            <div class="row">
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="icons/person-icon-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="icons/person-icon-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="row">
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="icons/person-icon-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="icons/person-icon-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="row">
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="icons/person-icon-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="icons/person-icon-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+
+    </div>
+
+</div>
+
+@endsection
---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,29 @@
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/sample2.jpg" class="img-fluid w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/sample3.jpg" alt="...">
+    </div>
+    {{-- <div class="carousel-item">
+      <img src="images/slider3.webp" class="mw-50" alt="...">
+    </div> --}}
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+
+<style>
+  .carousel-item {
+    height: 300px;
+    background-position: center center;
+    background-repeat: no-repeat;
+  }
+</style>
